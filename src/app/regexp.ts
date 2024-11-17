c7e471e3ba092ea6e1bae1fe3b0406d3f26a56db
Ronald <dev@ronaldtorrez.com>
2024-11-17 18:09:04 -0400

    feat: add regex constants for alphanumeric, whitespace, and file extensions

---
+++ src/app/regexp.ts
@@ -0,0 +1,4 @@
+export const ALPHANUMERIC_COLON_PAIR_REGEX: RegExp = /\b[\dA-Za-z]+:[\dA-Za-z]+\b/
+export const WHITESPACE_GLOBAL_REGEX: RegExp = /\s+/g
+export const NON_ALPHANUMERIC_GLOBAL_REGEX: RegExp = /[^\dA-Za-z]/g
+export const FILE_EXTENSION_REGEX: RegExp = /\.[^./]+$/
