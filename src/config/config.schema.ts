7387745992af090d19988763b03eab3efbabada3
Ronald <dev@ronaldtorrez.com>
2024-11-22 00:16:09 -0400

    refactor: enhance config schema with namespace key and update file config logic

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -1,18 +1,27 @@
-import { _FileConfigSchema } from '@/file/file-config.schema'
-import { _GetFilePathsSchema } from '@/file-paths/file-paths.schema'
+import { DEFAULT_NAMESPACE_KEY } from '@/app/const.app'
+import { _CosmiconfigSchema } from '@/cosmiconfig/cosmiconfig.schema'
+import { _FilePathsSchema } from '@/file-paths/file-paths.schema'
 import { _LoggerConfigSchema } from '@/logger/logger.schema'
+import { _StringNoNSchema } from '@/utils/string/string.schema'
 import { ZodAccelerator } from '@duplojs/zod-accelerator'
 import { z } from 'zod'
 
 const _i18nMergeConfigSchema = z
     .object({
-        filePaths: _GetFilePathsSchema.pick({
-            ignore: true,
-            paths: true,
-        }),
+        filePaths: _FilePathsSchema
+            .pick({
+                ignore: true,
+                paths: true,
+            })
+            .extend({
+                namespaceKey: _StringNoNSchema
+                    .default(DEFAULT_NAMESPACE_KEY)
+                    .describe('The key to use for the default namespace.'),
+            })
+            .describe('The paths to search for i18n files.'),
     })
     // @ts-ignore
-    .merge(_FileConfigSchema.pick({ loaders: true }))
+    .merge(_CosmiconfigSchema.pick({ loaders: true }))
     .merge(_LoggerConfigSchema.pick({ logLevel: true }))
 
 export type i18nMergeConfig = z.input<typeof _i18nMergeConfigSchema>
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,16 +1,16 @@
 import { CONFIG_FILE_NAME } from '@/app/const.app'
 import { i18nMergeConfig, i18nMergeConfigSchema } from '@/config/config.schema'
-import { initFileConfig } from '@/file/file-config'
+import { initCosmiconfig } from '@/cosmiconfig/cosmiconfig'
 import { initLoggerConfig } from '@/logger/logger'
 import { CosmiconfigResult, PublicExplorer } from 'cosmiconfig'
 
 export async function loadConfig(): Promise<i18nMergeConfig> {
-    const fileConfig: PublicExplorer = initFileConfig({
+    const cosmiconfig: PublicExplorer = initCosmiconfig({
         fileName: CONFIG_FILE_NAME,
         logger: initLoggerConfig({}),
     })
 
-    const searchConfig: CosmiconfigResult = await fileConfig.search()
+    const searchConfig: CosmiconfigResult = await cosmiconfig.search()
 
     return i18nMergeConfigSchema.parse(searchConfig?.config)
 }
