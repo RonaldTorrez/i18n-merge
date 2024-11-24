8bee9d9253c7106a913e9fc98193cb3c305f117e
Ronald <dev@ronaldtorrez.com>
2024-11-24 02:26:06 -0400

    refactor: update namespace schema in const.app.ts
    Renamed DEFAULT_NAMESPACE_KEY to DEFAULT_NAMESPACE_SCHEMA and updated
    its value from '__namespace__' to '__NS__'.

--- src/app/const.app.ts
+++ src/app/const.app.ts
@@ -1,3 +1,3 @@
 export const NODE_MODULE: string = 'node_modules'
 export const CONFIG_FILE_NAME: string = 'i18n-marge'
-export const DEFAULT_NAMESPACE_KEY: string = '__namespace__'
+export const DEFAULT_NAMESPACE_SCHEMA: string = '__NS__'
