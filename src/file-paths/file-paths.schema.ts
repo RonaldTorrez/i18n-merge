c9a4adeb70f6399c3912445d9b05dfc0d656f173
Ronald <dev@ronaldtorrez.com>
2024-11-24 02:27:52 -0400

    refactor: simplify file-paths schema and remove NODE_MODULE default

--- src/file-paths/file-paths.schema.ts
+++ src/file-paths/file-paths.schema.ts
@@ -1,19 +1,20 @@
-import { NODE_MODULE } from '@/app/const.app'
 import { _LoggerSchema } from '@/logger/logger.schema'
 import { _StringNoNArrayNoNSchema, _StringNoNArraySchema } from '@/utils/string/string.schema'
 import { ZodAccelerator } from '@duplojs/zod-accelerator'
 import { z } from 'zod'
 
-export const _FilePathsSchema = _LoggerSchema.extend({
-    ignore: _StringNoNArraySchema
-        .default([ NODE_MODULE ])
-        .describe(
-            'An array of strings specifying the paths to ignore in the search.'),
-    paths: _StringNoNArrayNoNSchema
-        .describe(
-            'An array of strings specifying the paths to include',
-        ),
-})
+// TODO Rename to SearchFiles or similar
+export const _FilePathsSchema = _LoggerSchema
+    .extend({
+        ignore: _StringNoNArraySchema
+            .default([])
+            .describe(
+                'An array of strings specifying the paths to ignore in the search.'),
+        paths: _StringNoNArrayNoNSchema
+            .describe(
+                'An array of strings specifying the paths to include',
+            ),
+    })
 
 export type FilePaths = z.input<typeof _FilePathsSchema>
 
