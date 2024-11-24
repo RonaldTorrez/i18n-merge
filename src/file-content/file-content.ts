c845606c9549e4948dd32ab1722c09aa0ab50e73
Ronald <dev@ronaldtorrez.com>
2024-11-24 12:37:12 -0400

    feat: add FilesContentReturn schema and enhance getFilesContent
    Introduced a new schema `FilesContentReturnSchema` to structure file content
    returns. Updated `getFilesContent` to return structured data using the new
    schema and improved type safety.

--- src/file-content/file-content.ts
+++ src/file-content/file-content.ts
@@ -1,26 +1,27 @@
 import { initCosmiconfig } from '@/cosmiconfig/cosmiconfig'
 import { FileContent, FileContentSchema } from '@/file-content/file-content.schema'
+import { FilesContentReturn } from '@/file-content/file-content-return.schema'
 import { isEmpty } from '@/utils/validation/is-empty'
 import { CosmiconfigResult, PublicExplorer } from 'cosmiconfig'
 
 export async function getFilesContent(
     parameters: FileContent,
-) {
+): Promise<FilesContentReturn[]> {
     const { loaders, logger, paths } = FileContentSchema.parse(parameters)
-    const cosmiconfig: PublicExplorer = initCosmiconfig({ logger })
+    const cosmiconfig: PublicExplorer = initCosmiconfig({ loaders, logger })
 
     const contents = await Promise.all(
         paths.map(async ({ name, path }) => {
             const loadContent: CosmiconfigResult = await cosmiconfig.load(path)
 
-            if (isEmpty(loadContent?.config)) {
+            if (!loadContent || isEmpty(loadContent.config)) {
                 return
             }
 
             return {
-                content: loadContent?.config,
+                content: loadContent.config,
                 filename: name,
-                path: loadContent?.filepath,
+                path: loadContent.filepath,
             }
         }),
     )
