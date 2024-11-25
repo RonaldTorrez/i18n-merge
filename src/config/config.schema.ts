d6b98ae265cc651121a87226b74418d72eaf092a
Ronald <dev@ronaldtorrez.com>
2024-11-24 21:34:29 -0400

    refactor: streamline i18n schema structure and naming
    - Renamed `_i18nMergeConfigFileSchemaSchema` to `_i18nFileSchemaSchema` for
      clarity.
    - Updated `_i18nMergeConfigFilesSchema` to `_i18nFilesSchema`.
    - Simplified schema definitions and descriptions for improved readability.
    - Integrated `namespaceSchema` into `_i18nFileSchemaSchema` with a default
      value.

--- src/config/config.schema.ts
+++ src/config/config.schema.ts
@@ -6,26 +6,20 @@ import { _StringNoNSchema } from '@/utils/string/string.schema'
 import { ZodAccelerator } from '@duplojs/zod-accelerator'
 import { z } from 'zod'
 
-export const _i18nMergeConfigFileSchemaSchema = z.object({
-    schema: z
-        .object({
-            namespace: _StringNoNSchema
-                .describe(
-                    'The key to use for the default namespace.'),
-        })
-        .default({
-            namespace: DEFAULT_NAMESPACE_SCHEMA,
-        })
+export const _i18nFileSchemaSchema = z.object({
+    namespaceSchema: _StringNoNSchema
+        .default(DEFAULT_NAMESPACE_SCHEMA)
         .describe(
-            'The schema to use for the files options.'),
+            'The key to use for the default namespace.'),
 })
 
-export const _i18nMergeConfigFilesSchema = z.object({
+export const _i18nFilesSchema = z.object({
     files: _SearchFilePathsSchema
         .pick({
             ignore: true,
             paths: true,
         })
+
         .extend({
             ignoreNodeModules: z
                 .boolean()
@@ -33,7 +27,9 @@ export const _i18nMergeConfigFilesSchema = z.object({
                 .describe(
                     'If true, the node_modules directory will be ignored.'),
         })
-        .extend(_i18nMergeConfigFileSchemaSchema.shape)
+        .extend(_i18nFileSchemaSchema.shape)
+        .describe('The paths to search for i18n files.')
+
         .transform((value) => {
             if (value.ignoreNodeModules) {
                 value.ignore.push(NODE_MODULE)
@@ -45,14 +41,13 @@ export const _i18nMergeConfigFilesSchema = z.object({
             ...value,
             ignore: [ ...new Set(value.ignore) ],
         }),
-        )
-        .describe('The paths to search for i18n files.'),
+        ),
 })
 
 const _i18nMergeConfigSchema = _CosmiconfigSchema
     .pick({ loaders: true })
     .extend(_LoggerConfigSchema.pick({ logLevel: true }).shape)
-    .extend(_i18nMergeConfigFilesSchema.shape)
+    .extend(_i18nFilesSchema.shape)
 
 export type i18nMergeConfig = z.input<typeof _i18nMergeConfigSchema>
 export const i18nMergeConfigSchema = ZodAccelerator.build(_i18nMergeConfigSchema)
